0ca7be9ae96e7b5d4050a89b122f7702ca6c756f
ณัฐรินทร์ อินมณี <s6002041510039@Macmini-213-24.local>
2019-03-21 13:21:12 +0700

    new component

---
+++ component/App.js
@@ -0,0 +1,18 @@
+//import Library
+import React from 'react';
+import { View , Text } from 'react-native';
+
+//write component
+class App extends React.Component{
+    render() {
+        return (
+            //component about react native
+            <View>
+                <Text> Nattarin</Text>
+            </View>
+        )
+    }
+}
+
+//export
+export default App;
