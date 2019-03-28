e8b0833322de37dd135956a9ce40eaa35a31bcc2
ณัฐรินทร์ อินมณี <s6002041510039@Macmini-213-24.local>
2019-03-28 14:14:15 +0700

    new component

--- component/Header.js
+++ component/Header.js
@@ -3,5 +3,13 @@ import React, { component} from 'react';
 import { View, Text } from 'react-native';
 
 // creat component
-
+class Header extends component {
+    render() {
+        return (
+            <View>
+                <Text>Albums</Text>
+            </View>
+        )
+    }
+}
 //export
