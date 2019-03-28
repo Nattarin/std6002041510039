9ed26a88a5083c1695ccb74675e7e704b1ef8655
ณัฐรินทร์ อินมณี <s6002041510039@Macmini-213-24.local>
2019-03-28 16:23:19 +0700

    creat const

--- component/Card.js
+++ component/Card.js
@@ -7,11 +7,13 @@ class Card extends Component {
         return (
            <View>
                <CardSection>
-                <Image
-                    style={{width: 100, height: 100 }}
-                    source={{uri: 'https://images.unsplash.com/photo-1513569026477-146b9135d2ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80'}}
-                />
-                <Text>Car Classic</Text>
+                   <View style={styles.images}>
+                        <Image
+                            style={{width: 100, height: 100 }}
+                            source={{uri: 'https://images.unsplash.com/photo-1513569026477-146b9135d2ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80'}}
+                        />
+                         <Text>Car Classic</Text>
+                    </View>
                 
                </CardSection>
                <CardSection>
@@ -20,6 +22,7 @@ class Card extends Component {
                     source={{uri: 'https://images.unsplash.com/photo-1512562848786-ed86b318eab6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80'}}
                 />
                </CardSection>
+
                <CardSection>
                    <View style={styles.button}>
                     <View style={{ width: 100, backgroundColor: 'red' }}>
@@ -29,6 +32,7 @@ class Card extends Component {
                     </View>
                    </View>
                </CardSection>
+               
            </View> 
         );
     }
@@ -39,6 +43,10 @@ const styles = {
         justifyContent: 'center', 
         alignItems: 'center', 
         marginTop: 10 
+    },
+    images: {
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 }
 
