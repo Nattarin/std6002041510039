4505369bd3babbb680bea232c692da0fce52e16c
ณัฐรินทร์ อินมณี <s6002041510039@Macmini-213-24.local>
2019-04-11 15:17:13 +0700

    new

--- component/App.js
+++ component/App.js
@@ -2,51 +2,34 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Header from './Header';
-<<<<<<< HEAD
 */
 // import Card from './Card';
-=======
-//import Card from './Card';
 import Login from './Login';
->>>>>>> 9b0161b0c61c26e1dece6ae8fb1c2b4a99b38e51
-
-import Login from './Login';
-import Me from './Me';
+import About from './About';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
+
 
 /*
 export default class App extends React.Component {
     render() {
         return (
             <View>
-<<<<<<< HEAD
                 <Header name="อัลบั้ม" />
                 <View style={{ marginTop: 20 }}>
                     <Card />
                 </View>
-=======
-                <Header title="Login" />
-                <Login/>
->>>>>>> 9b0161b0c61c26e1dece6ae8fb1c2b4a99b38e51
             </View>
         );
     }
 }
-<<<<<<< HEAD
 */
 
-const AppNavigator = createStackNavigator(
-    {
-        Login: Login,
-        Me: Me
-    }, {
-        // initialRouteName: "Login"
-        initialRouteName: "Me"
-    }
-);
+const AppNavigator = createStackNavigator({
+    Login: Login,
+    About : About
+}, {
+    // initialRouteName: "Login"
+    initialRouteName: "About"
+});
 
 export default createAppContainer(AppNavigator);
-=======
-//export
-export default App;
->>>>>>> 9b0161b0c61c26e1dece6ae8fb1c2b4a99b38e51
